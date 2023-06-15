000af759dae0a5a1178d5fa07217b424b5c408ec
khushi mehta <khushimehta8282@gmail.com>
2023-06-15 20:41:46 +0530

    cursor pointer

--- src/components/Avail.js
+++ src/components/Avail.js
@@ -14,14 +14,14 @@ function Avail() {
             {/* <Typography variant='h2' sx={{ fontWeight: 'bold', fontFamily: 'Poppins', margin: '0', padding: '0', fontSize: { md: '4rem', sm: '2rem', xs: '2rem' } }}><strong style={{ color: '#3770FF' }}>Consumers</strong> Get connected to organizations to sell your content</Typography> */}
             <div data-aos="fade-up">
                 <Grid container spacing={3} sx={{ marginTop: '5%' }}>
-                    <Grid item  md={4} sm={6} xs={12} sx={{ marginBottom: { md: '0', sm: '0', xs: '10%' } }} >
+                    <Grid onClick={() => window.open('https://app.myreeldream.ai/')} item  md={4} sm={6} xs={12} sx={{ marginBottom: { md: '0', sm: '0', xs: '10%' }, cursor:'pointer' }} >
                         <CardContent sx={{ backgroundColor: 'white', height: '100%', borderRadius: '20px', boxShadow: 'rgba(235, 235, 235, 0.77) 0px 7px 29px 0px' }}>
                             {/* <CardMedia component='img' image={avail1} sx={{ height: '100px', width: 'auto', marginTop: '-4rem' }} /> */}
                             <Icon icon="eva:bulb-outline" style={{ height: '100px', width: 'auto', marginTop: '-4rem', color:'#3770FF' }} />
 
                             <Grid container style={{display:'flex' , justifyContent:'center', alignItems:'center'}}>
                             <Grid item md={12}>
-                            <h2 style={{ margin: '0', padding: '0', display:'flex', alignItems:'center', marginBottom:'5%' }}>Ideation <ArrowForwardIcon onClick={() => window.open('https://app.myreeldream.ai/')} sx={{ cursor: 'pointer', display: { md: 'flex', xs: 'none' } }} /></h2>
+                            <h2 style={{ margin: '0', padding: '0', display:'flex', alignItems:'center', marginBottom:'5%' }}>Ideation <ArrowForwardIcon  sx={{ cursor: 'pointer', display: { md: 'flex', xs: 'none' } }} /></h2>
                             </Grid>
                             <Grid item md={1}>
                             <Icon icon="charm:tick-double" width={28} height={28} color="#3770FF5c"/>
