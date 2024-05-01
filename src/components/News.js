db3f209a8b90f342a489e944fa1d3a584a3423bf
saudkhan <saudkhanbpk@gmail.com>
2024-05-01 02:21:54 -0700

    news slider added and some functionalities added

--- src/components/News.js
+++ src/components/News.js
@@ -1,40 +1,43 @@
 import React, { useState } from 'react';
 import { Box, Typography, makeStyles } from '@material-ui/core';
+import "./News.css"
+import Slider from 'react-slick';
+import { BorderTop, Padding } from '@mui/icons-material';
 
 const useStyle = makeStyles((theme) => ({
     news: {
         color: "white",
-        marginBottom:"30px",
-        display:"flex",
-        gap:"10px",
-        fontSize:"20px"
-        
+        marginBottom: "30px",
+        display: "flex",
+        gap: "10px",
+        fontSize: "20px"
+
 
     },
     empty: {
-    border:"1px solid gray",
-    height:"0px",
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center",
-    width:"100%",
-    marginTop:"35px"
+        border: "1px solid gray",
+        height: "0px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        marginTop: "35px"
 
     },
     text: {
-        width:"45%",
-        fontFamily:"mono",
-        fontSize:"20px",
-        marginTop:'-28px',
+        width: "45%",
+        fontFamily: "mono",
+        fontSize: "20px",
+        marginTop: '-28px',
         [theme.breakpoints.down('sm')]: {
-            textAlign:"center",
-            width:"100%"
-        
+            textAlign: "center",
+            width: "100%"
+
         },
     },
     videoContainer1: {
         display: "flex",
-        flexWrap:"wrap",
+        flexWrap: "wrap",
         gap: "20px",
         justifyContent: "center",
         // width: "100%",
@@ -42,26 +45,51 @@ const useStyle = makeStyles((theme) => ({
         // height: "auto",
         marginBottom: "3%",
         [theme.breakpoints.down('sm')]: {
-        
-        marginBottom: "0"
+
+            marginBottom: "0"
         },
     },
     video1: {
-    width:"530px",
+        width: "530px",
         border: "0px",
-        height:"320px",
+        height: "320px",
         [theme.breakpoints.down('md')]: {
             borderRadius: 0,
             width: '100%',
-            
+
 
         },
     },
+    carasoul: {
+        // width:"380px",
+        paddingTop:'10px',
+        paddingLeft:"12px",
+        // border:'3px solid white',
+        // transition: 'border-color 0.3s ease-in-out', // Smooth transition for border color change
+        '&:hover': {
+            BorderTop: '3px solid white'
+        }
+    }
 }));
 
 function Video() {
     const [isHovered2, setIsHovered2] = useState(false);
-
+    const [mainVideo, setMainVideo] = useState("https://www.youtube.com/embed/n53ZxxwJxA0");
+    const [mainTitle,setMainTitle]=useState("CHINE'S SUBMARINE GAME CHANGER")
+       const [mainText, setMainText] = useState(` Our platform is governed by a decentralized network of smart contracts,
+       ensuring fairness, integrity, and accountability in all interactions.
+       By decentralizing decision-making and governance processes, smart contracts empower users to participate in shaping the future of our platform,
+        fostering a sense of ownership, community, and collective responsibility.`);
+        const [expanded, setExpanded] = useState(false);
+       const handleCarouselItemClick = (videoLink, para, title) => {
+           setMainVideo(videoLink);
+           setMainText(para);
+           setMainTitle(title);
+       };
+       const toggleExpand = () => {
+        setExpanded(!expanded);
+    };
+  
 
     const handleMouseEnter2 = () => {
         setIsHovered2(true);
@@ -73,6 +101,43 @@ function Video() {
 
     const classes1 = useStyle();
 
+    const settings = {
+
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true, // Autoplay slides
+    autoplaySpeed: 3000, // Delay between autoplay slides (in milliseconds)
+    arrows: false,
+    };
+    const setting = {
+
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+    const paragraph = [
+        {
+            para: `The unknown story of Greek philosopher Plato’s death may have been unveiled from a newly deciphered ancient scroll, according to Italian researchers.`,
+            link: "https://www.youtube.com/embed/7cjmjOj7JlA",
+            title: `Scrolls discovered in Vesuvius ash reveal Plato’s burial place and final hours`,
+        },
+        {
+            para: `Prime Minister Benjamin Netanyahu has said Israel will launch an invasion of the southern Gaza city of Rafah regardless of truce talks with Hamas.`,
+            link: "https://www.youtube.com/embed/1PkO4tUcUOk" ,
+            title: `Israel-Gaza war: UN chief calls Israel's airstrikes in Rafah an ‘unbearable escalation’`,
+        },
+        {
+            para: `In August 2020, Sviatlana Tsikhanouskaya was elected president of Belarus.
+            Tsikhanouskaya then fled to Lithuania in 2020 after President Alexander Lukashenko deployed a severe crackdown on protests against his disputed election win.`,
+            link: "https://www.youtube.com/embed/UumnYvQvOTg",
+            title: `Ukraine war: Exiled Belarus opposition leader speaks out against Russia’s threat `
+        }
+    ]
 
     return (
         <Box>
@@ -88,7 +153,7 @@ function Video() {
                     {isHovered2 ? (<iframe
                         title="YouTube Video"
                         className={classes1.video1}
-                        src="https://www.youtube.com/embed/n53ZxxwJxA0?autoplay=1&mute=1&controls=0"
+                        src={`${mainVideo}?autoplay=1&mute=1&controls=0`}
                         allow="autoplay; encrypted-media"
                         allowFullScreen
                     ></iframe>
@@ -96,7 +161,7 @@ function Video() {
                         <iframe
                             title="YouTube Video"
                             className={classes1.video1}
-                            src="https://www.youtube.com/embed/n53ZxxwJxA0?autoplay=&mute=1&controls=0"
+                            src={`${mainVideo}?autoplay=&mute=1&controls=0`}
                             allow="autoplay; encrypted-media"
                             allowFullScreen
                         ></iframe>
@@ -104,17 +169,71 @@ function Video() {
                 </div>
                 <div className={classes1.text}>
                     <h2>
-                        CHINE'S SUBMARINE GAME CHANGER
+            {mainTitle}
                     </h2>
-                <h4 >
-                    Our platform is governed by a decentralized network of smart contracts, ensuring fairness, integrity, and accountability in all interactions. By decentralizing decision-making and governance processes, smart contracts empower users to participate in shaping the future of our platform, fostering a sense of ownership, community, and collective responsibility.
-                </h4>
+                    <h4 >
+                       {mainText}
+                    </h4>
                 </div>
-              
+
 
             </div>
+            <div className='news1'>
 
-            {/* Your other content here */}
+                <Slider {...settings} style={{display:'flex', width: '100%', justifyContent:'center' }}>
+
+                    {paragraph.map((item, index) => {
+                        return <div className={classes1.carasoul} onClick={() => handleCarouselItemClick(item.link, item.para, item.title)} key={index}>
+                            <iframe
+                                style={{ width: '360px' }}
+                                title="YouTube Video"
+                                src={item.link}
+                                allow="autoplay; encrypted-media"
+                                picture-in-picture
+                                allowFullScreen
+                            />
+                            <Typography variant='h5' style={{ color: 'white' }}>{item.title}</Typography>
+                            <Typography style={{ color: 'white' }}>
+                            {expanded ? item.para : `${item.para.slice(0, 50)}...`}
+                            <span onClick={toggleExpand} style={{ cursor: 'pointer', marginLeft: '5px' }}>
+                                {expanded ? 'See Less' : 'See More'}
+                            </span>
+                            </Typography>
+                        </div>
+                    })
+
+                    }
+                </Slider>
+            </div>
+            <div className='news2'>
+
+                <Slider {...setting} style={{display:'flex', width: '90%', justifyContent:'center', }}>
+
+                    {paragraph.map((item, index) => {
+                        return <div className='box' onClick={() => handleCarouselItemClick(item.link, item.para, item.title)} key={index}>
+                            <iframe
+                                style={{ width:'100%' }}
+                                title="YouTube Video"
+                                src={item.link}
+                                allow="autoplay; encrypted-media"
+                                picture-in-picture
+                                allowFullScreen
+                            />
+                            <Typography variant='h5' style={{ color: 'white' }}>{item.title}</Typography>
+                            <Typography style={{ color: 'white' }}>
+                            {expanded ? item.para : `${item.para.slice(0, 50)}...`}
+                            <span onClick={toggleExpand} style={{ cursor: 'pointer', marginLeft: '5px' }}>
+                                {expanded ? 'See Less' : 'See More'}
+                            </span>
+                            </Typography>
+                        </div>
+                    })
+
+                    }
+                </Slider>
+            </div>
+
+
         </Box>
     );
 }
